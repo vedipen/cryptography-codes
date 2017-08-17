4215371880d4ced45c86ac1cb8e752caae084381
Dipen Ved <vedipen@gmail.com>
2017-08-17 22:47:13 +0530

    Complete Alice and Bob

--- Man-In-The-Middle/Alice.java
+++ Man-In-The-Middle/Alice.java
@@ -4,26 +4,60 @@ import java.io.*;
 
 public class Alice extends Thread {
   private ServerSocket serverSocket;
+  public static int g=6,p=13;
+  private static int x=3;
 
   public Alice(int port) throws IOException {
     serverSocket = new ServerSocket(port);
     serverSocket.setSoTimeout(30000);
   }
 
+  public int generateKey(int r2) {
+      return (int)(Math.pow(r2,x))%p;
+  }
+
+  public String encrypt(String in,int k) {
+    String out="";
+    for(int i=0;i<in.length();i++) {
+      out+=((char)(((in.charAt(i)-'a'+k)%26)+'a')+"").toUpperCase();
+    }
+    return out;
+  }
+
+  public int findR1() {
+    int r1=(int)(Math.pow(g,x))%p;
+    System.out.println("R1 generated is "+r1);
+    return r1;
+  }
+
   public void run() {
-    while(true) {
+    Boolean over=false;
+    while(!over) {
       try {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Waiting for client on port " +
         serverSocket.getLocalPort() + "...");
         Socket server = serverSocket.accept();
 
         System.out.println("Just connected to " + server.getRemoteSocketAddress());
         DataInputStream in = new DataInputStream(server.getInputStream());
-
-        System.out.println(in.readUTF());
+        System.out.println("Sending R1 to Bob");
+        int r1=findR1();
         DataOutputStream out = new DataOutputStream(server.getOutputStream());
-        out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress()
-        + "\nGoodbye!");
+        out.writeUTF(r1+"");
+        int r2=Integer.parseInt(in.readUTF());
+        System.out.println("R2 recieved from Bob is " + r2);
+        int k = generateKey(r2);
+        System.out.println("Generated key is "+k);
+        System.out.print("Enter message to send : ");
+        String input = br.readLine();
+        String output = encrypt(input,k);
+        System.out.println("Generated Cipher is : " + output);
+        System.out.println("Sending Cipher to Bob..");
+        out.writeUTF(output);
+        // out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress()
+        // + "\nGoodbye!");
+        over=true;
         server.close();
 
       } catch(SocketTimeoutException s) {
@@ -38,6 +72,9 @@ public class Alice extends Thread {
 
   public static void main(String [] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Public Key p is : "+p);
+    System.out.println("Public Key g is : "+g);
+    System.out.println("Private Key of Alice is : "+x);
     System.out.print("Enter the port you want to connect to : ");
     int port = Integer.parseInt(br.readLine());
     try {
--- Man-In-The-Middle/Bob.java
+++ Man-In-The-Middle/Bob.java
@@ -4,8 +4,36 @@ import java.io.*;
 
 public class Bob {
 
+  public static int g=6,p=13;
+  private static int y=10;
+
+  public static int generateKey(int r1) {
+      return (int)(Math.pow(r1,y))%p;
+  }
+
+  public static String decrypt(String in,int k) {
+    String out="";
+    for(int i=0;i<in.length();i++) {
+      int z=(in.toLowerCase().charAt(i)-'a'-k+26);
+      if(z>=25) {
+        z%=26;
+      }
+      out+=((char)(z+'a')+"");
+    }
+    return out;
+  }
+
+  public static int findR2() {
+    int r1=(int)(Math.pow(g,y))%p;
+    System.out.println("R1 generated is "+r1);
+    return r1;
+  }
+
   public static void main(String [] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Public Key p is : "+p);
+    System.out.println("Public Key g is : "+g);
+    System.out.println("Private Key of Bob is : "+y);
     String serverName="localhost";
     // System.out.print("Enter Server Name to connect to (default - 'localhost') : ");
     // String serverNameTemp = br.readLine();
@@ -19,14 +47,22 @@ public class Bob {
       Socket client = new Socket(serverName, port);
 
       System.out.println("Just connected to " + client.getRemoteSocketAddress());
-      OutputStream outToServer = client.getOutputStream();
-      DataOutputStream out = new DataOutputStream(outToServer);
-
-      out.writeUTF("Hello from " + client.getLocalSocketAddress());
       InputStream inFromServer = client.getInputStream();
       DataInputStream in = new DataInputStream(inFromServer);
-
-      System.out.println("Server says " + in.readUTF());
+      OutputStream outToServer = client.getOutputStream();
+      DataOutputStream out = new DataOutputStream(outToServer);
+      int r2=findR2();
+      int r1 = Integer.parseInt(in.readUTF());
+      System.out.println("R1 recieved from Alice is "+r1);
+      System.out.println("Sending R2 to Alice");
+      out.writeUTF(r2+"");
+      // out.writeUTF("Hello from " + client.getLocalSocketAddress());
+      int k = generateKey(r1);
+      System.out.println("Generated key is "+k);
+      String cipher = in.readUTF();
+      System.out.println("Cipher recieved is " + cipher);
+      String plainText = decrypt(cipher,k);
+      System.out.println("Message sent from Alice is "+plainText);
       client.close();
     }catch(IOException e) {
       System.out.println("Errrr! Couldn't connect. Stats -->");
